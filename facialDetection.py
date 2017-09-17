b8176e63e1869bec5384b3cb44e11637b8ffb581
pssudhaka <psudhaka1997@gmail.com>
2017-09-17 01:40:36 -0400

    This is after adding the detection

--- facialDetection.py
+++ facialDetection.py
@@ -6,3 +6,7 @@ print 'here'
 img=cv2.imread('lelouch.jpg')
 
 print 'image load works'
+
+while True:
+    grey_color=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    face_detecct=variable.detectMultiScale(grey_color, 1.3, 5)
