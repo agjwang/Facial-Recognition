794f56b5a15f519d43e112037c7f8fac687dbb2c
pssudhaka <psudhaka1997@gmail.com>
2017-09-17 01:59:37 -0400

    added classification code and created output

--- facialDetection.py
+++ facialDetection.py
@@ -7,6 +7,15 @@ img=cv2.imread('lelouch.jpg')
 
 print 'image load works'
 
-while True:
-    grey_color=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    face_detecct=variable.detectMultiScale(grey_color, 1.3, 5)
+grey=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+face_detect=variable.detectMultiScale(grey, 1.3, 5)
+for(x,y,w,h) in face_detect:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    roi_gray = grey[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+
+cv2.imshow('img',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+print 'it works'
