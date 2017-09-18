94622e606d0ea55daaf53b3f9378fc7a36956955
Alex Wang <alexander.gj.wang@gmail.com>
2017-09-18 00:05:52 -0400

    Added more functionality to overlaying images on top of recognized faces

--- imageCreator.py
+++ imageCreator.py
@@ -1,16 +1,33 @@
+import numpy as np
+import cv2
+
 from PIL import Image
 from facialDetection import classifyFaces
 
 def overlayImage(image_path, overlay_path, output_path):
-   face_data = classifyFaces(image_path)
-   overlay = Image.open(overlay_path)
-   image = Image.open(image_path)
+    face_data = classifyFaces(image_path)
+    image = cv2.imread(image_path)
 
-   overlay = resizeImage(overlay, face_data)
-   Image.alpha_composite(image, overlay).save(output_path)
+    overlay = cv2.imread(overlay_path, -1)
+    new_image = image
 
-def resizeImage(image, face_data)
-   image.thumbnail(face_data['w'])
-   image.transform(image.size, Image.AFFINE, (1, 0, face_data['x'], 0, 1, face_data['y']))
+    for face in face_data:
+        x, y, h, w = face['x'], face['y'], face['h'], face['w']
+        overlay = resizeOverlay(overlay, face)
 
-   return image
+        for c in range(0, 3):
+            alpha = overlay[:, :, 3] / 255.0
+            overlay_values = overlay[:, :, c] * alpha
+            background_values = image[y : y + h, x : x + h, c] * (1 - alpha)
+            image[y : y + h, x : x + w, c] = overlay_values + background_values
+
+        cv2.rectangle(new_image, (face['x'], face['y']), (face['x'] + face['w'], face['y'] + face['h']), (255, 0, 0), 2)
+
+    cv2.imwrite(output_path, new_image)
+    return
+
+def resizeOverlay(overlay, face_data):
+    overlay = cv2.resize(overlay, (face_data['w'], face_data['h']))
+    return overlay
+
+overlayImage('lelouch.jpg', 'dark_magician_girl_hat.png', 'new.jpg')
